4a13d0b33c960b46aa1c99abf5388d8a0ec5eca1
nvtd20112005 <165932302+nvtd20112005@users.noreply.github.com>
2025-04-02 14:34:19 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Menu
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+const overlay = document.querySelector("#overlay");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+const backToTopButton = document.querySelector(".back-to-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
